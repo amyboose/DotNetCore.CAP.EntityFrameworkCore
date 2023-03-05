c6a23f1a5b29b3972b7eebf1a3d06ecc2e61207b
Denis <amyboose1@gmail.com>
2023-03-05 15:30:26 +0500

    0

--- src/DotNetCore.CAP.EntityFrameworkCore/ServiceCollectionExtensions.cs
+++ src/DotNetCore.CAP.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -1,14 +1,13 @@
 ﻿// Copyright (c) .NET Core Community. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using DotNetCore.CAP;
-using DotNetCore.CAP.EntityFrameworkCore;
 using DotNetCore.CAP.EntityFrameworkCore.Interceptors;
 using DotNetCore.CAP.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 // ReSharper disable once CheckNamespace
-namespace Microsoft.Extensions.DependencyInjection;
+namespace DotNetCore.CAP.EntityFrameworkCore;
 
 /// <summary>
 /// Contains extension methods to <see cref="IServiceCollection" /> for configuring consistence services.
